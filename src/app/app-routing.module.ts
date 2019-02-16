6f0b1808ce582927f1726210ba71ada04fabf35b
zh-konopleova <konopleova@gmail.com>
2019-02-16 23:08:13 +0300

    Add components header, authorization, registration, question-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuestionListComponent } from './question-list/question-list.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: QuestionListComponent},
+  {path: 'login', component: AuthorizationComponent},
+  {path: 'signup', component: RegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { QuestionListComponent } from './question-list/question-list.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    QuestionListComponent,
+    AuthorizationComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
